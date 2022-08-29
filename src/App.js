5e1af6f81a1ab67ec7ee8608a667302724437221
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-08-29 13:41:29 -0700

    updates to ui/ux of site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import dino from './dino_image.jpg'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Name Carbon Dater</h3>
+        <h6>Enter your name and press submit to see what age we think you might be!</h6>
+        <img src={dino} id="dino-logo" alt="dinosaur logo" />
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <Button id="submit-button" variant="contained">Submit</Button>
+        
       </header>
     </div>
   );
