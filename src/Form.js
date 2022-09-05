635115260177c2011f8eb0f5bf60e8ce1631d184
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-09-05 15:10:19 -0700

    multiple names syntax working, results component broken

--- src/Form.js
+++ src/Form.js
@@ -6,18 +6,23 @@ import TextField from '@mui/material/TextField';
 
 
 //addition of country param? Addition of multiple names?
-const Results = ({age}) => {
+//results assume one object size, so need to expand results to take in multiple dictionaries in <Results/> component
+//for loop to display divs 
+
+
+const Results = ({name, age}) => {
     return (<div 
     id="returned-API-data"
-    aria-label="space to display age based on input name">
-         We think you are {age}. 
+    aria-label="space to display age based on input name(s) and country">
+         We think {name} is {age}. 
     </div>
 )};
 
 const Form = (props) => {
-    const [nameObject, setNameObject] = useState({name: "", age: "", count: ""});
+    const [agifyData, setAgifyData] = useState([]);
 	const [country, setCountry] = useState({country_id: ""});
-	const[isHovering, setIsHovering] = useState(false);
+	const [isHovering, setIsHovering] = useState(false);
+	const [names, setNames] = useState([]);
 
 	const handleMouseOver = () => {
 		setIsHovering(true);
@@ -29,30 +34,34 @@ const Form = (props) => {
 
     const handleNameInput = (event) =>{
         event.preventDefault();
-        let username = event.target.value;
-        setNameObject((nameObject) => ({...nameObject, name:username}));
+        let nameString = event.target.value;
+        setNames((x) => (nameString.split(",")));
     }
 	const handleCountryInput = (event) =>{
 		event.preventDefault();
 		let country_id = event.target.value;
-		setCountry((country) => ({...country, country_id:country_id}));
+		setCountry((x) => ({...x, country_id:country_id}));
 	}
 
     // GET request 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const URL = `https://api.agify.io?name=${nameObject.name}&${country.country_id}`;
-		// const URL2 = `https://api.agify.io?name[]=${nameObject.name}`
-        fetch(URL)
+        let URL = "https://api.agify.io?";
+		for(let name of names){
+			URL = URL.concat(`name[]=${name}&`)
+		}
+		//how to deal with dangling ampersand, last element of string? Get rid of last element
+        URL = country.country_id ? URL.concat(`country_id=${country.country_id}`) : URL;
+		let trailingAmpersandURL = URL.endsWith("&");
+		URL = trailingAmpersandURL ? URL.slice(0, -1) : URL;
+		console.log(names, URL)
+		fetch(URL)
         .then(response => response.json())
         .then(data => {
-            setNameObject(data);		
-			console.log(data)
+            setAgifyData(data);		
             // this.props.onSubmit(data);
         })
-
-    
-}
+	};
 
 
   return (
@@ -61,9 +70,9 @@ const Form = (props) => {
     <Grid container alignItems="center" justify="center" direction="column">
         <Grid item >
       <TextField 
-      id="outlined-basic"
-      label="Name"
-      sx={{
+      	id="outlined-basic"
+      	label="Name(s)"
+    	sx={{
     	"& label": {
           width: "90%",
           transformOrigin: "center",
@@ -75,19 +84,19 @@ const Form = (props) => {
 			textAlign: "center"
 		  }
       }}
-      value={nameObject.name}
-      onChange={handleNameInput}
-      variant="outlined" 
-      aria-label="text feild in which you can input your name"
+    	value={agifyData.name}
+    	onChange={handleNameInput}
+    	variant="outlined" 
+    	aria-label="text feild in which you can input your name"
       >
     </TextField>	
       </Grid>
 	  {isHovering && <a id="small-text-window" href="https://agify.io/our-data" target="blank">How Do I Input my Country?</a>}
 	  <Grid item>  
 	<TextField 
-      id="outlined-basic"
-      label="Country"
-      sx={{
+    	id="outlined-basic"
+    	label="Country"
+    	sx={{
     	"& label": {
           width: "90%",
           transformOrigin: "center",
@@ -99,27 +108,34 @@ const Form = (props) => {
 			textAlign: "center"
 		  }
       }}
-      value={country.country_id}
-      onChange={handleCountryInput}
-	  onMouseOver={handleMouseOver}
-	  onMouseOut={handleMouseOut}
-      variant="outlined" 
-      aria-label="text feild in which you can input your country"
+    	value={country.country_id}
+    	onChange={handleCountryInput}
+		onMouseOver={handleMouseOver}
+		onBlur={handleMouseOut}
+    	variant="outlined" 
+    	aria-label="text feild in which you can input your country"
       ></TextField>
 	  </Grid>
 	  <Button 
-      id="submit-button" 
-      variant="contained"
-      type="submit"
-      onClick={handleSubmit}
-      aria-label="button to submit form">
+    	id="submit-button" 
+    	variant="contained"
+    	type="submit"
+    	onClick={handleSubmit}
+    	aria-label="button to submit form">
         Submit
       </Button>
       </Grid>
     </form>
-    { nameObject.age ? <Results age={nameObject.age}/> : null } 
+	for(i of i){
+    <Results 
+	name={agifyData.name}
+	age={agifyData.age}
+	/> 
+	}
   </div>
   )
 }
 
 export default Form;
+
+//for loop in jsx, get rid of ternary
