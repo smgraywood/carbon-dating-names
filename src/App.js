a1660fea9150ab0f7f1f79040d70a5dc8fca111d
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-09-08 19:19:53 -0700

    fixing formatting errors with prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.css';
-import dino from './dino_image.jpg'
-import Form from './Form.js'
+import React from "react";
+import "./App.css";
+import dino from "./dino_image.jpg";
+import Form from "./Form.js";
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Name Carbon Dater</h1>
-        <h6 id="user-message">Enter your name and press submit to see what age we think you might be!</h6>
+        <h6 id="user-message">
+          Enter your name and press submit to see what age we think you might
+          be!
+        </h6>
         <div id="image-wrapper">
           <img src={dino} id="dino-logo" alt="dinosaur logo" />
         </div>
       </header>
-        < Form />
+      <Form />
     </div>
   );
 }
